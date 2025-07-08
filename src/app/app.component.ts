e0ece9ce3891209a1678f4eb260ac7052c2363fb
Ivan CM <56263882+Ivan21A7x@users.noreply.github.com>
2025-07-08 11:49:03 -0600

    RXJS-logic-added-for-Star-Wars-characters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   combineLatest,
+  debounceTime,
   filter,
   forkJoin,
   map,
@@ -8,6 +9,7 @@ import {
   Subject,
   Subscription,
   switchMap,
+  takeUntil,
 } from 'rxjs';
 import { MockDataService } from './mock-data.service';
 
@@ -23,6 +25,8 @@ export class AppComponent implements OnInit, OnDestroy {
   isLoading: boolean = false;
   subscriptions: Subscription[] = [];
 
+  private destroy$ = new Subject<void>();
+
   constructor(private mockDataService: MockDataService) {}
 
   ngOnInit(): void {
@@ -33,47 +37,52 @@ export class AppComponent implements OnInit, OnDestroy {
   changeCharactersInput(element: any): void {
     // 1.1. Add functionality to changeCharactersInput method. Changes searchTermByCharacters Subject value on input change.
     const inputValue: string = element.target.value;
-    // YOUR CODE STARTS HERE
-
-    // YOUR CODE ENDS HERE
+    this.searchTermByCharacters.next(inputValue);
   }
 
   initCharacterEvents(): void {
     // 1.2. Add API call on each user input. Use mockDataService.getCharacters - to make get request.
+    this.charactersResults$ = this.searchTermByCharacters.pipe(
+      filter((value) => value.length >= 3),
 
-    // 2. Since we don't want to spam our service add filter by input value and do not call API until a user enters at least 3 chars.
+      debounceTime(300),
 
-    // 3. Add debounce to prevent API calls until user stop typing.
-
-    this.charactersResults$ = this.searchTermByCharacters
-        .pipe
-        // YOUR CODE STARTS HERE
-
-        // YOUR CODE ENDS HERE
-        ();
+      switchMap((searchTerm) => this.mockDataService.getCharacters(searchTerm))
+    );
   }
 
   loadCharactersAndPlanet(): void {
     // 4. On clicking the button 'Load Characters And Planets', it is necessary to process two requests and combine the results of both requests into one result array. As a result, a list with the names of the characters and the names of the planets is displayed on the screen.
-    // Your code should looks like this: this.planetAndCharactersResults$ = /* Your code */
-    // YOUR CODE STARTS HERE
-    // YOUR CODE ENDS HERE
+    this.planetAndCharactersResults$ = forkJoin([
+      this.mockDataService.getCharacters(),
+      this.mockDataService.getPlanets(),
+    ]).pipe(
+      map(([characters, planets]) => [...characters, ...planets])
+    );
   }
 
   initLoadingState(): void {
-    /* 5.1. Let's add loader logic to our page. For each request, we have an observable that contains the state of the request. When we send a request the value is true, when the request is completed, the value becomes false. You can get value data with mockDataService.getCharactersLoader() and mockDataService.getPlanetLoader().
+    /* 5.1. Let's add loader logic to our page. For each request, we have an observable that contains the state of the request. When we send a request the value is true, when the request is completed, the value becomes false. You can get value data with mockDataService.getCharactersLoader() and mockDataService.getPlanetLoader().*/
+    const loading$ = combineLatest([
+      this.mockDataService.getCharactersLoader(),
+      this.mockDataService.getPlanetLoader(),
+    ]).pipe(
+      map(([charLoader, planetLoader]) => 
+        this.areAllValuesTrue([charLoader, planetLoader]),
+      )
+    );
+    const sub = loading$.subscribe((loading) => {
+      this.isLoading = loading;
+    });
 
-    - Combine the value of each of the streams.
-    - Subscribe to changes
-    - Check the received value using the areAllValuesTrue function and pass them to the isLoading variable. */
-    // YOUR CODE STARTS HERE
-    // YOUR CODE ENDS HERE
+    this.subscriptions.push(sub);
   }
 
   ngOnDestroy(): void {
     // 5.2 Unsubscribe from all subscriptions
-    // YOUR CODE STARTS HERE
-    // YOUR CODE ENDS HERE
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   areAllValuesTrue(elements: boolean[]): boolean {
